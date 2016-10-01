a04ec24e2264992bd0c02232c59eccd591f85e17
Tobias <Tobias@CAMELONTA-PC>
2016-10-01 22:00:30 +0200

    Initialize codemirror

--- UmbracoXmlEdit/App_Plugins/UmbracoXmlEdit/BackOffice/content/edit-controller.js
+++ UmbracoXmlEdit/App_Plugins/UmbracoXmlEdit/BackOffice/content/edit-controller.js
@@ -1,9 +1,32 @@
 'use strict';
-(function() {
-    // Create controller
-    function contentController($scope, $http, notificationsService) {
+(function () {
+    // Register controller
+    angular.module("umbraco").controller("XmlEdit.ContentController", ['$scope', '$http', '$timeout', 'notificationsService', function ($scope, $http, $timeout, notificationsService) {
+        function initCodemirror() {
+            // Set timeout to make sure value is bound before we initialize codemirror
+            $timeout(function () {
+                // Initialize codemirror
+                if (typeof (CodeMirror) != 'undefined') {
+                    var textArea = document.getElementById('xmledit-content');
+
+                    // Only init if value is set when XML is loaded, otherwise it'll won't be shown at all
+                    if (textArea.value) { 
+                        var codeEditor = CodeMirror.fromTextArea(textArea, {
+                            tabMode: "shift",
+                            matchBrackets: true,
+                            indentUnit: 4,
+                            indentWithTabs: true,
+                            enterMode: "keep",
+                            lineWrapping: false,
+                            lineNumbers: true
+                        });
+                    }
+                }
+            }, 10);
+        }
+
         var updateModelValue = function (response) {
-            var xml = JSON.parse(response.data); 
+            var xml = JSON.parse(response.data);
             $scope.model.value = xml;
         }
 
@@ -16,17 +39,18 @@
             method: 'GET',
             url: 'backoffice/XmlEdit/content/getxml',
             params: { nodeId: currentNodeId }
-        }).then(function(response) {
+        }).then(function (response) {
             updateModelValue(response);
+            initCodemirror();
         }, function errorCallback(response) {
             notificationsService.error("Couldn't get XML");
         });
 
-        $scope.hideDialog = function() {
+        $scope.hideDialog = function () {
             $scope.nav.hideDialog(true);
         }
 
-        $scope.saveXml = function() {
+        $scope.saveXml = function () {
             var data = {
                 nodeId: currentNodeId,
                 xml: $scope.model.value
@@ -36,7 +60,7 @@
                 method: 'POST',
                 url: 'backoffice/XmlEdit/content/savexml',
                 data: data
-            }).then(function(response) {
+            }).then(function (response) {
                 notificationsService.success('Successfully saved XML');
                 updateModelValue(response);
                 $scope.hideDialog();
@@ -46,8 +70,5 @@
                 notificationsService.error("Couldn't save XML");
             });
         }
-    }
-
-    // Register controller
-    angular.module("umbraco").controller("XmlEdit.ContentController", contentController);
+    }]);
 })();
